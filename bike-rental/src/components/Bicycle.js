275c27bf9ddf4ed7a418b162c0763fded6024f9f
Ihor Koss <r.wallace2206@gmail.com>
2020-03-27 02:45:37 +0200

    add bikes array

--- bike-rental/src/components/Bicycle.js
+++ bike-rental/src/components/Bicycle.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import bikesData from './bikesData';
 
 export default function Bicycle() {
   return (       
---
+++ bike-rental/src/components/bikesData.js
@@ -0,0 +1,74 @@
+const bikesData = [
+    {
+        id: 1001,
+        bike_name: "Surerfast bicycle",
+        bike_type: "Custom",
+        rent_price: 12.99,
+        rent: true
+    },
+    {
+        id: 1002,
+        bike_name: "Surerfast bicycle",
+        bike_type: "Custom",
+        rent_price: 12.99,
+        rent: false
+    },
+    {
+        id: 1003,
+        bike_name: "Surerfast bicycle",
+        bike_type: "Custom",
+        rent_price: 12.99,
+        rent: false
+    },
+    {
+        id: 1004,
+        bike_name: "Surerfast bicycle",
+        bike_type: "Custom",
+        rent_price: 12.99,
+        rent: false
+    },
+    {
+        id: 1005,
+        bike_name: "Victoria",
+        bike_type: "Mountain",
+        rent_price: 19.99,
+        rent: false
+    },
+    {
+        id: 1006,
+        bike_name: "Victoria",
+        bike_type: "Mountain",
+        rent_price: 19.99,
+        rent: false
+    },
+    {
+        id: 1007,
+        bike_name: "Victoria",
+        bike_type: "Mountain",
+        rent_price: 19.99,
+        rent: false
+    },
+    {
+        id: 1008,
+        bike_name: "Gnomik",
+        bike_type: "Road",
+        rent_price: 15.99,
+        rent: false
+    },
+    {
+        id: 1009,
+        bike_name: "Gnomik",
+        bike_type: "Road",
+        rent_price: 15.99,
+        rent: false
+    },
+    {
+        id: 1010,
+        bike_name: "Gnomik",
+        bike_type: "Road",
+        rent_price: 15.99,
+        rent: false
+    }
+];
+
+export default bikesData;
